a6b072ccf5816d0aa753cd4bbd3e5633ab99bb77
eunsonny <eunsonnny@gmail.com>
2023-09-29 22:20:27 +0900

    Add: yarn workspace를 이용한 모노레포 구조로 변경

---
+++ apps/example/src/App.tsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+import { Button } from '@eunsonny/ui'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <Button>버튼</Button>
+      
+    </>
+  )
+}
+
+export default App
---
+++ packages/library/rollup.config.mjs
@@ -0,0 +1,60 @@
+import resolve from '@rollup/plugin-node-resolve';
+import babel from "@rollup/plugin-babel";
+import typescript from "@rollup/plugin-typescript";
+import postcss from 'rollup-plugin-postcss'
+import commonjs from '@rollup/plugin-commonjs'
+import peerDepsExternal from 'rollup-plugin-peer-deps-external';
+import path from 'path'
+
+export default {
+  input: {
+    index: path.join('./src', 'index')
+  },
+  output: ['cjs', 'esm'].map(getOutputOption),
+  external: [/@babel\/runtime/, /.yarn/],
+  // external: [/@babel\/runtime/],
+  plugins: [
+    commonjs(),
+    resolve({ extensions: ['.js', '.jsx', '.ts', '.tsx'] }), 
+    babel({
+      babelHelpers: "runtime",
+      exclude: ['.yarn/**'],
+      presets: [
+        "@babel/preset-env",
+        "@babel/preset-react",
+        "@babel/preset-typescript",
+      ],
+      extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      plugins: ['@babel/plugin-transform-runtime']
+    }),
+    typescript({
+      include: ['./src/**/*.{ts,tsx}'],
+      exclude: [
+        'node_modules',
+        '**/stories',
+        '**/*.stories.{ts,tsx}'
+      ]
+    }),
+    postcss({
+      config: {
+        path: './postcss.config.js'
+      },
+      extensions: ['.css'], 
+      extract: true,
+    }),
+    peerDepsExternal(),
+  ],
+};
+
+/** @type {() => import('rollup').OutputOptions} */
+function getOutputOption(format) {
+  return {
+    dir: './package',
+    sourcemap: true,
+    format,
+    entryFileNames: `[name].${format}.js`,
+    interop: 'auto',
+    preserveModules: true,
+    preserveModulesRoot: './src',
+  };
+}
